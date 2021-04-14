aeccbc75cbc6213c9e9e76e097d8d7cf60338549
dzikieszosy <77523791+dzikieszosy@users.noreply.github.com>
2021-04-14 16:18:43 +0200

    project_3 -> working forEach ul li

--- projekt_3/main.js
+++ projekt_3/main.js
@@ -1,1 +1,16 @@
 // rozwiązanie
+
+const button = document.querySelector("button");
+const li = document.querySelectorAll("li");
+let size = 10;
+
+
+
+// forEach version
+button.addEventListener("click", function () {
+   li.forEach(function (element) {
+      element.style.display = "block";
+      element.style.fontSize = size + "px";
+   })
+   size += 1;
+})
