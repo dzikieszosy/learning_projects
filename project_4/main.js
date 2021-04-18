7c7e4d364e3f94495cf57e953b5235807b8e2877
dzikieszosy <77523791+dzikieszosy@users.noreply.github.com>
2021-04-18 18:03:03 +0200

    universal method

--- project_4/main.js
+++ project_4/main.js
@@ -1,7 +1,25 @@
-document.body.addEventListener("mousemove", (event) => {
-   // console.log(event.clientX, event.clientY);
-   document.querySelector('h1').textContent = `${event.clientX}, ${event.clientY}`; // visible page or whole page
-   // document.querySelector('h1').textContent = `${event.pageX}, ${event.pageY}`;
+// document.body.addEventListener("mousemove", (event) => {
+//    // console.log(event.clientX, event.clientY);
+//    document.querySelector('h1').textContent = `${event.clientX}, ${event.clientY}`; // visible page or whole page
+//    // document.querySelector('h1').textContent = `${event.pageX}, ${event.pageY}`;
 
-   document.body.style.backgroundColor = `rgb(${event.clientX/3},${event.clientY/2},${event.clientX/event.clientY*20})`;
+//    document.body.style.backgroundColor = `rgb(${event.clientX/3},${event.clientY/2},${event.clientX/event.clientY*20})`;
+// })
+
+// second universal method
+
+const h1 = document.querySelector('h1');
+document.body.addEventListener('mousemove', function (e) {
+   const x = e.clientX + 1;
+   const y = e.clientY + 1;
+   const width = window.innerWidth;
+   const height = window.innerHeight;
+   h1.textContent = x + ", " + y;
+
+   const red = x / width * 100;
+   const green = y / height * 100;
+   const blue = ((x / width * 100) + (y / height * 100)) / 2;
+
+   document.body.style.backgroundColor = `rgb(${red}%, ${green}%, ${blue}%)`
+
 })
