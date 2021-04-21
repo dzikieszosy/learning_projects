61a1ad68665ea14bcf1e4d491626756c73cb156e
dzikieszosy <77523791+dzikieszosy@users.noreply.github.com>
2021-04-21 09:07:51 +0200

    drag and drop works but only one point

--- project_4_3/main.js
+++ project_4_3/main.js
@@ -1,1 +1,27 @@
 // drag and drop square
+const div = document.querySelector('div');
+let divX = 150;
+let divY = 50;
+div.style.left = divX + "px"; //this way
+div.style.top = `${divY}px`; // or this way
+
+let drawActive = false;
+
+div.addEventListener('mousedown', () => {
+   div.style.backgroundColor = 'gray';
+   drawActive = !drawActive;
+})
+
+div.addEventListener('mousemove', (e) => {
+   if (drawActive) {
+      divX = e.clientX;
+      divY = e.clientY;
+      div.style.left = `${divX-50}px`;
+      div.style.top = `${divY-50}px`;
+   }
+})
+
+div.addEventListener('mouseup', () => {
+   div.style.backgroundColor = 'black';
+   drawActive = !drawActive;
+})
