2a01abea8a2201f6a641a8cbbbcafd3e309b704b
dzikieszosy <77523791+dzikieszosy@users.noreply.github.com>
2021-04-21 14:40:18 +0200

    map method, for instead forEach

--- project_5_2/main.js
+++ project_5_2/main.js
@@ -1,20 +1,44 @@
+// map method
+
 const input = document.querySelector("input");
 const passwords = ['jedEN', 'DwA'];
-const messages = ["super", "works!"]
+const messages = ["super", "works!"];
+const div = document.querySelector('div');
 
-passwords.forEach((password, index) => {
-   passwords[index] = password.toLowerCase();
-   console.log(password);
-})
+const LCPasswords = passwords.map(password => password.toLowerCase());
 
 const showMessage = (e) => {
-   const input = e.target.value.toLowerCase();
-   passwords.forEach((password, i) => {
-      if (password === input) {
-         // console.log('Victory');
-         document.querySelector('div').textContent = messages[i];
+   const textInput = e.target.value.toLowerCase();
+
+   for (let i = 0; i < LCPasswords.length; i++) {
+      if (textInput === LCPasswords[i]) {
+         div.innerHTML = messages[i];
       }
-   })
+   }
 }
 
 input.addEventListener("input", showMessage)
+
+
+// first basic method
+
+// const input = document.querySelector("input");
+// const passwords = ['jedEN', 'DwA'];
+// const messages = ["super", "works!"]
+
+// passwords.forEach((password, index) => {
+//    passwords[index] = password.toLowerCase();
+//    console.log(password);
+// })
+
+// const showMessage = (e) => {
+//    const input = e.target.value.toLowerCase();
+//    passwords.forEach((password, i) => {
+//       if (password === input) {
+//          // console.log('Victory');
+//          document.querySelector('div').textContent = messages[i];
+//       }
+//    })
+// }
+
+// input.addEventListener("input", showMessage)
