aa2852d1922970d8ab7557c9e7de4e5c1669bc9e
dzikieszosy <77523791+dzikieszosy@users.noreply.github.com>
2021-04-21 11:45:06 +0200

    message and img display completed

--- project_5/main.js
+++ project_5/main.js
@@ -1,1 +1,19 @@
-// 
+// password message prject
+
+const input = document.getElementById('pass');
+
+const password = "user";
+const message = "Please hire me! ;-) Have a good day!!!";
+const div = document.querySelector('.message');
+// const img = document.getElementById('please');
+
+input.addEventListener('input', (e) => {
+   // console.log(e.target.value);
+   if (password === e.target.value) {
+      div.textContent = message;
+      document.getElementById('please').style.display = "inline-block";
+      // const NewImg = document.createElement('NewImg');
+      // NewImg.src = "/img/hireme.jpg";
+      // document.getElementById('please').appendChild(NewImg);
+   }
+})
