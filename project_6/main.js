a1ca88b74737a4076674c93e9e08df31ea21bea4
dzikieszosy <77523791+dzikieszosy@users.noreply.github.com>
2021-04-24 11:40:52 +0200

    Names generator working

--- project_6/main.js
+++ project_6/main.js
@@ -1,1 +1,15 @@
 // random child name
+
+const btn = document.querySelector('button');
+const div = document.querySelector('div');
+
+const names = ["Jagienka", "Dobrawa", "Daria", "Asia", "Kira", "Marysia", "Anastazja"];
+const prefixs = ["Wydaje mi się", "Mam wrażenie", "Szczerze powiedziawszy, myślę", "Na 100% twierdzę", "Jestem pewien"]
+
+const nameGenerator = () => {
+   const index = Math.floor(Math.random() * names.length);
+   const prefixIndex = Math.floor(Math.random() * prefixs.length);
+   div.innerHTML = `${prefixs[prefixIndex]}, że najlepsze będzie imię <strong>${names[index]}</strong>`;
+}
+
+btn.addEventListener('click', nameGenerator)
