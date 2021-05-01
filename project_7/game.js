6be30c6ae78c4cab2fb19a6eb445980c2a808ad0
dzikieszosy <77523791+dzikieszosy@users.noreply.github.com>
2021-05-01 18:20:31 +0200

    hand selection

--- project_7/game.js
+++ project_7/game.js
@@ -1,1 +1,21 @@
-// Zobacz gotowy projekt: https://websamuraj.pl/examples/js/projekt7/
+const gameSummary = {
+   numbers: 0,
+   wins: 0,
+   losses: 0,
+   draws: 0,
+}
+
+const game = {
+   playerHand: "",
+   aiHand: ""
+}
+
+const hands = [...document.querySelectorAll('.select img')]; //[... coś] - operator rest zamienia na tablice
+
+function handSelection() {
+   game.playerHand = this.dataset.option;
+   hands.forEach(hand => hand.style.boxShadow = '');
+   this.style.boxShadow = '0 0 0 4px yellow';
+}
+
+hands.forEach(hand => hand.addEventListener('click', handSelection)); //pobiera jeden element z tablicy i uruchamia funkcje wybierania ręki
