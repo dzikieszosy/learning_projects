573149bc82f2af082dd9b6743979271276da6046
dzikieszosy <77523791+dzikieszosy@users.noreply.github.com>
2021-05-02 12:51:11 +0200

    game finished

--- project_7/game.js
+++ project_7/game.js
@@ -1,3 +1,5 @@
+// Zobacz gotowy projekt: https://websamuraj.pl/examples/js/projekt7/
+
 const gameSummary = {
    numbers: 0,
    wins: 0,
@@ -7,46 +9,82 @@ const gameSummary = {
 
 const game = {
    playerHand: "",
-   aiHand: ""
+   aiHand: "",
 }
 
-const hands = [...document.querySelectorAll('.select img')]; //[... coś] - operator rest zamienia na tablice
+const hands = [...document.querySelectorAll('.select img')];
 
-// wybór ręki
+// Pierwsza funkcja
 function handSelection() {
-   game.playerHand = this.dataset.option;
-   hands.forEach(hand => hand.style.boxShadow = ''); // pusty boxshadow
-   this.style.boxShadow = '0 0 0 4px yellow'; // wybieranie box shadow
+
+   game.playerHand = this.dataset.option
+   console.log(game.playerHand);
+   hands.forEach(hand => hand.style.boxShadow = '');
+   this.style.boxShadow = '0 0 0 4px red';
 }
 
-//losowanie komputera
+// const handSelection = (e) => {
+//  // this - nie tworzy
+//  console.log(e.target);
+//  console.log(e.currentTarget);
+// }
+
+// Funkcja określająca wybór komputera
 function aiChoice() {
-   return hands[Math.floor(Math.random() * 3)].dataset.option; //losowanie ręki ai
+   return hands[Math.floor(Math.random() * 3)].dataset.option;
 }
 
+// Funkcja zwracajająca informacje o wyniku gry
 function checkResult(player, ai) {
+   // console.log(player, ai);
    if (player === ai) {
       return 'draw';
-   } else if ((player === "papier" && ai === "kamień") ||
-      (player === "kamień" && ai === "nożyczki") ||
-      (player === "nożyczki" && ai === "papier")) {
+   } else if ((player === "papier" && ai === "kamień") || (player === "kamień" && ai === "nożyczki") || (player === "nożyczki" && ai === "papier")) {
       return 'win';
    } else {
       return 'loss';
    }
 }
 
-//funkcja sterująca
-function startGame() {
-   if (!game.playerHand) return alert("Wybierz dłoń");
+// Publikacja wyniku
 
-   game.aiHand = aiChoice()
-   const gameResult = checkResult(game.playerHand, game.aiHand);
+function publishResult(player, ai, result) {
+   document.querySelector('[data-summary="your-choice"]').textContent = player;
+
+   document.querySelector('[data-summary="ai-choice"]').textContent = ai;
+
+   document.querySelector('p.numbers span').textContent = ++gameSummary.numbers;
+
+   if (result === "win") {
+      document.querySelector('p.wins span').textContent = ++gameSummary.wins;
+      document.querySelector('[data-summary="who-win"]').textContent = "Ty wygrałeś!!!!"
+      document.querySelector('[data-summary="who-win"]').style.color = "green";
+   } else if (result === "loss") {
+      document.querySelector('p.losses span').textContent = ++gameSummary.losses;
+      document.querySelector('[data-summary="who-win"]').textContent = "Komputer wygrał :("
+      document.querySelector('[data-summary="who-win"]').style.color = "red";
+   } else {
+      document.querySelector('p.draws span').textContent = ++gameSummary.draws;
+      document.querySelector('[data-summary="who-win"]').textContent = "Remis :\\"
+      document.querySelector('[data-summary="who-win"]').style.color = "gray";
+   }
 }
 
+function endGame() {
+   document.querySelector(`[data-option="${game.playerHand}"]`).style.boxShadow = "";
+}
+//funkcja sterująca
+function startGame() {
+   if (!game.playerHand) {
+      return alert("wybierz dłoń!!!!");
+   }
+   game.aiHand = aiChoice();
+   const gameResult = checkResult(game.playerHand, game.aiHand);
+   console.log(gameResult);
+   publishResult(game.playerHand, game.aiHand, gameResult);
+   endGame();
+}
 
+hands.forEach(hand => hand.addEventListener('click', handSelection))
 
-//nasłuchiwanie
-hands.forEach(hand => hand.addEventListener('click', handSelection)); //pobiera jeden element z tablicy i uruchamia funkcje wybierania ręki
-
-document.querySelector('.start').addEventListener('click', startGame);
+document.querySelector('.start').addEventListener('click', startGame)
