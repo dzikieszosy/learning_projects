36802fb4c446257bcb172e609bb5a3bc54b7ed8c
dzikieszosy <77523791+dzikieszosy@users.noreply.github.com>
2021-05-02 15:13:48 +0200

    typing with cursor

--- project_8/main.js
+++ project_8/main.js
@@ -1,4 +1,5 @@
 const spnText = document.querySelector('.text');
+const spnCursor = document.querySelector('.cursor');
 const txt = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde eum, saepe neque reprehenderit modi sed quos consectetur accusantium, perferendis perspiciatis aperiam magnam soluta voluptatum obcaecati earum voluptas similique illo nihil!';
 
 let indexText = 0;
@@ -10,4 +11,9 @@ const addLetter = () => {
    if (indexText == txt.length) clearInterval(indexTyping); //clearinterval resetuje działanie interwału
 }
 
+const cursorAnimation = () => {
+   spnCursor.classList.toggle('active') //.przełącza klasy z css
+}
+
 const indexTyping = setInterval(addLetter, time); //setInterval zwraca index...
+setInterval(cursorAnimation, 400);
