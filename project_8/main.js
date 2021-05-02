94fa65227ba43eb58067213abcbf275fe20efbc3
dzikieszosy <77523791+dzikieszosy@users.noreply.github.com>
2021-05-02 14:35:22 +0200

    typing working

--- project_8/main.js
+++ project_8/main.js
@@ -1,1 +1,13 @@
-//
+const spnText = document.querySelector('.text');
+const txt = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde eum, saepe neque reprehenderit modi sed quos consectetur accusantium, perferendis perspiciatis aperiam magnam soluta voluptatum obcaecati earum voluptas similique illo nihil!';
+
+let indexText = 0;
+const time = 100;
+
+const addLetter = () => {
+   spnText.textContent += txt[indexText];
+   indexText++;
+   if (indexText == txt.length) clearInterval(indexTyping); //clearinterval resetuje działanie interwału
+}
+
+const indexTyping = setInterval(addLetter, time); //setInterval zwraca index...
