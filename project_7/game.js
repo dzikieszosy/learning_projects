2497b74e7072fe194b90f2e738d8ec51c3887641
dzikieszosy <77523791+dzikieszosy@users.noreply.github.com>
2021-05-02 12:15:15 +0200

    checkResult function completed

--- project_7/game.js
+++ project_7/game.js
@@ -19,13 +19,34 @@ function handSelection() {
    this.style.boxShadow = '0 0 0 4px yellow'; // wybieranie box shadow
 }
 
-//funkcja sterująca
-function startGame() {
-   if (game.playerHand === "") {
-      alert("Wybierz dłoń");
+//losowanie komputera
+function aiChoice() {
+   return hands[Math.floor(Math.random() * 3)].dataset.option; //losowanie ręki ai
+}
+
+function checkResult(player, ai) {
+   if (player === ai) {
+      return 'draw';
+   } else if ((player === "papier" && ai === "kamień") ||
+      (player === "kamień" && ai === "nożyczki") ||
+      (player === "nożyczki" && ai === "papier")) {
+      return 'win';
+   } else {
+      return 'loss';
    }
 }
 
+//funkcja sterująca
+function startGame() {
+   if (!game.playerHand) return alert("Wybierz dłoń");
+
+   game.aiHand = aiChoice()
+   const gameResult = checkResult(game.playerHand, game.aiHand);
+}
+
+
+
+//nasłuchiwanie
 hands.forEach(hand => hand.addEventListener('click', handSelection)); //pobiera jeden element z tablicy i uruchamia funkcje wybierania ręki
 
 document.querySelector('.start').addEventListener('click', startGame);
