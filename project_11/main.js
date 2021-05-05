69d1d1961786333926725a79f18d09d708c99b01
dzikieszosy <77523791+dzikieszosy@users.noreply.github.com>
2021-05-05 18:20:33 +0200

    simple countdown finished

--- project_11/main.js
+++ project_11/main.js
@@ -1,10 +1,36 @@
-const clock = () => {
-   const time = new Date();
-   const seconds = time.getSeconds() < 10 ? "0" + time.getSeconds() : time.getSeconds();
-   const minutes = time.getMinutes() < 10 ? "0" + time.getMinutes() : time.getMinutes();
-   const hours = time.getHours() < 10 ? "0" + time.getHours() : time.getHours();
+// const clock = () => {
+//    const time = new Date();
+//    const seconds = time.getSeconds() < 10 ? "0" + time.getSeconds() : time.getSeconds();
+//    const minutes = time.getMinutes() < 10 ? "0" + time.getMinutes() : time.getMinutes();
+//    const hours = time.getHours() < 10 ? "0" + time.getHours() : time.getHours();
 
-   document.querySelector('.clock span').textContent = `${hours}:${minutes}:${seconds}`;
-}
+//    document.querySelector('.clock span').textContent = `${hours}:${minutes}:${seconds}`;
+// }
 
-setInterval(clock, 1000)
+// setInterval(clock, 1000)
+
+//zwraca wartość milisekund od 1.1.1970
+const endTime = new Date('2021-12-31 23:59:59').getTime();
+
+const spanD = document.querySelector('span.d');
+const spanH = document.querySelector('span.h');
+const spanM = document.querySelector('span.m');
+const spanS = document.querySelector('span.s');
+
+setInterval(() => {
+      const nowTime = new Date().getTime();
+      // const time = Math.floor((endTime - nowTime) / 1000); //zwraca liczbe w s
+
+      const time = endTime - nowTime;
+      const days = Math.floor((endTime / (1000 * 60 * 60 * 24)) - (nowTime / (1000 * 60 * 60 * 24)));
+      const hours = Math.floor((endTime / (1000 * 60 * 60) - nowTime / (1000 * 60 * 60)) % 24);
+      const minutes = Math.floor((endTime / (1000 * 60) - nowTime / (1000 * 60)) % 60);
+      const seconds = Math.floor((endTime / 1000 - nowTime / 1000) % 60);
+
+
+      spanD.textContent = days;
+      spanH.textContent = hours;
+      spanM.textContent = minutes;
+      spanS.textContent = seconds;
+   },
+   1000)
