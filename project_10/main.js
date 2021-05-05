8d31c54544b8621237cdd5dd821b0d3687e4dbbb
dzikieszosy <77523791+dzikieszosy@users.noreply.github.com>
2021-05-05 13:42:45 +0200

    simple counter finished - closure

--- project_10/main.js
+++ project_10/main.js
@@ -1,1 +1,9 @@
-//
+const add = (start = 0) => {
+   let number = start;
+   return () => {
+      number++;
+      document.body.textContent = `Aktualny stan licznika kliknięć to: ${number}`;
+   }
+}
+const counter = add();
+document.addEventListener('click', counter);
