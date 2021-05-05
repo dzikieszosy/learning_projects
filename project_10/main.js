491440d8e129e64ece8a9e926dc45bf4a873fb62
dzikieszosy <77523791+dzikieszosy@users.noreply.github.com>
2021-05-05 16:58:39 +0200

    simple age verify by closures on the same project

--- project_10/main.js
+++ project_10/main.js
@@ -1,9 +1,29 @@
-const add = (start = 0) => {
-   let number = start;
-   return () => {
-      number++;
-      document.body.textContent = `Aktualny stan licznika kliknięć to: ${number}`;
+// simple counter
+
+// const add = (start = 0) => {
+//    let number = start;
+//    return () => {
+//       number++;
+//       document.body.textContent = `Aktualny stan licznika kliknięć to: ${number}`;
+//    }
+// }
+// const counter = add();
+// document.addEventListener('click', counter);
+
+// verify age
+
+const user = (name = "", age) => {
+   let userName = name;
+   let userAge = age;
+
+   function showName() {
+      console.log(`Cześć ${userName}, ${userAge >= 18 ? 'Możesz kupić piwo' : 'Nie możesz kupić piwa'}`);
    }
+   return showName;
 }
-const counter = add();
-document.addEventListener('click', counter);
+
+const mieszko = user("Mieszko", 20);
+const jagienka = user("Jagienka", 17);
+
+mieszko();
+jagienka();
