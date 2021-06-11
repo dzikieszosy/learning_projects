f2b74efe06acbc457e9f74d01f4d85c5b6ebc6b1
dzikieszosy <77523791+dzikieszosy@users.noreply.github.com>
2021-06-11 08:38:06 +0200

    project 12 - index.html and main.js phase 1

--- project_12/main.js
+++ project_12/main.js
@@ -1,1 +1,8 @@
-//
+const removeTask = (e) => {
+   // console.log(event.target.textContent); //pobieranie tekstu z poszczególnych li
+   // e.target.parentNode.remove(); // usuwanie poszczególnych li za pomocą przycisku (parentNode)
+   e.target.parentNode.style.textDecoration = "line-through"; // przekreślenie elementu parentNode
+   e.target.remove(); // wywala przycisk "usuń"
+}
+
+document.querySelectorAll('li button').forEach(item => item.addEventListener('click', removeTask)) // nasłuchiwanie na wszystkie button w li
